a09c104d9cff31b07a71794eb334d5c52aab1b5c
andyjknakamura <73955056+andyjknakamura@users.noreply.github.com>
2022-11-16 21:33:18 -0500

    task 1

--- discussion11.py
+++ discussion11.py
@@ -39,12 +39,13 @@ def create_species_table(cur, conn):
 # TASK 1
 # CREATE TABLE FOR PATIENTS IN DATABASE
 def create_patients_table(cur, conn):
-    pass
-
+    cur.execute("CREATE TABLE IF NOT EXISTS Patients (pet_id INTEGER, name STRING, species_id INTEGER, age INTEGER, cuteness INTEGER, aggressiveness INTEGER)")
+    conn.commit()
 
 # ADD FLUFFLE TO THE TABLE
 def add_fluffle(cur, conn):
-    pass
+    cur.execute("INSERT INTO Patients (pet_id, name, species_id, age, cuteness, aggressiveness) VALUES (0, 'Fluffle', 0, 3, 90, 100)")
+    conn.commit()
     
 
 # TASK 2
@@ -59,7 +60,13 @@ def add_pets_from_json(filename, cur, conn):
     json_data = json.loads(file_data)
 
     # THE REST IS UP TO YOU
-    pass
+    print(json_data)
+    count = 0
+    for i in json_data:
+        count += 1
+        temp_id = cur.execute("SELECT id FROM Species WHERE title = i['species']")
+        cur.execute("INSERT INTO Patients (pet_id, name, species_id, age, cuteness, aggressiveness) VALUES (count, i['name'], temp_id, i['age'], i['cuteness'], i['aggressiveness'])")
+        conn.commit()
 
 
 # TASK 3
@@ -69,9 +76,10 @@ def non_aggressive_pets(aggressiveness, cur, conn):
 
 
 
+
 def main():
     # SETUP DATABASE AND TABLE
-    cur, conn = setUpDatabase('animal_hospital.db')
+    cur, conn = setUpDatabase('animal_hospital2.db')
     create_species_table(cur, conn)
 
     create_patients_table(cur, conn)
